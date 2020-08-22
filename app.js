a4b8ebbe30e4f4a8826bfea757cc23a5be8b0b27
Furkan Tural <furkantural3328@hotmail.com>
2020-08-22 15:20:30 +0300

    optimize code but still need more modification bu it work well

--- app.js
+++ app.js
@@ -1,78 +1,41 @@
-function gethistory()
-{
-    return document.getElementById('history-value').innerText;
+const getİnput = () => document.querySelector('.input-value').innerText;
+const printİnput = (n) => document.querySelector('.input-value').innerText = n;
+const getHistory = () => document.querySelector('.history-value').innerText;
+const printHistory = (n) => document.querySelector('.history-value').innerText = n;
+
+const number = document.getElementsByClassName('number');
+for(let i = 0; i < number.length; i++){
+    number[i].addEventListener('click', function(){
+        let input = getİnput();
+        input = input + this.id;//new num add to next to current value on screan
+        printİnput(input)// it prevent to over write each print
+    })
 }
-function printHistory(num){
-	document.getElementById("history-value").innerText=num;
-}
-function getOutput(){
-	return document.getElementById("output-value").innerText;
-}
-function printOutput(num){
-	if(num==""){
-		document.getElementById("output-value").innerText=num;
-	}
-	else{
-		document.getElementById("output-value").innerText=getFormattedNumber(num);
-    }	
-    function getFormattedNumber(num){
-        if(num=="-"){
-            return "";
+
+const operator = document.getElementsByClassName('operator');
+for(let i = 0; i < operator.length; i++){
+    operator[i].addEventListener('click', function(){
+        if(this.id == 'clear'){
+            printİnput('');
+            printHistory('');
         }
-        var n = Number(num);
-        var value = n.toLocaleString("en");
-        return value;
-    }
-    function reverseNumberFormat(num){
-        return Number(num.replace(/,/g,''));
-    }
-    var operator = document.getElementsByClassName("operator");
-    for(var i =0;i<operator.length;i++){
-        operator[i].addEventListener('click',function(){
-            if(this.id=="clear"){
-                printHistory("");
-                printOutput("");
+        else if(this.id == 'backspace'){
+            let input = getİnput();
+            input = input.substr(0,input.length-1);
+            printİnput(input)
+        }
+        else{
+            input = getİnput()
+            let history = getHistory()
+            history = history + input;
+            if(this.id == '='){
+                    printİnput('')
+                    printHistory(eval(history))
+            }else{
+                history=history+this.id;
+                printHistory(history);
+                printİnput('')
             }
-            else if(this.id=="backspace"){
-                var output=reverseNumberFormat(getOutput()).toString();
-                if(output){//if output has a value
-                    output= output.substr(0,output.length-1);
-                    printOutput(output);
-                }
-            }
-            else{
-                var output=getOutput();
-                var history=getHistory();
-                if(output==""&&history!=""){
-                    if(isNaN(history[history.length-1])){
-                        history= history.substr(0,history.length-1);
-                    }
-                }
-                if(output!="" || history!=""){
-                    output= output==""?output:reverseNumberFormat(output);
-                    history=history+output;
-                    if(this.id=="="){
-                        var result=eval(history);
-                        printOutput(result);
-                        printHistory("");
-                    }
-                    else{
-                        history=history+this.id;
-                        printHistory(history);
-                        printOutput("");
-                    }
-                }
-            }
-            
-        });
-    }
-    var number = document.getElementsByClassName("number");
-    for(var i =0;i<number.length;i++){
-        number[i].addEventListener('click',function(){
-            var output=reverseNumberFormat(getOutput());
-            if(output!=NaN){ //if output is a number
-                output=output+this.id;
-                printOutput(output);
-            }
-        });
-    }
+        }
+    })
+}
